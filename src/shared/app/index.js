0025f38c9e266ee22c7e1995f11c7d73255d51a5
roy <roy@digitalxi.com>
2018-06-07 14:36:17 -0400

    remove demo

--- src/shared/app/index.js
+++ src/shared/app/index.js
@@ -39,13 +39,7 @@ const App = ({ alertBar }) => (
                 path="/Dashboard" 
                 component={Routes.DashboardPage} 
             />
-
-            {/* <Route path="/playlists/:playlistId(pl-[a-z]{0,4})" component={Routes.PlaylistPage} />
-            <Route path="/playlists" component={Routes.PlayListsPage} />
-            <Route path="/search-album" component={Routes.SearchAlbumPage} />
-            <Route path="/albums/:albumSlug" component={Routes.AlbumPage} />
-            <Route path="/demo" component={Routes.DemoPage} /> */}
-
+            
             <Route path="*" render={() => (<Redirect to="/" />)} /> 
         </Switch>
 
--- src/shared/app/routes.js
+++ src/shared/app/routes.js
@@ -1,13 +1,5 @@
 import loadable from 'loadable-components';
 
 // Load the pieces of pages
-export const AlbumPage = loadable(() => import('../albums/AlbumPage'));
-export const HomePage = loadable(() => import('../home/HomePage'));
-export const PlaylistPage = loadable(() => import('../playlists/PlaylistPage'));
-export const PlayListsPage = loadable(() => import('../playlists/ListPage'));
-export const SearchAlbumPage = loadable(() => import('../albums/SearchPage'));
-export const DemoPage = loadable(() => import('../demo/DemoPage'));
-
-
 export const LoginPage = loadable(() => import('../login/LoginPage'));
 export const DashboardPage = loadable(() => import('../dashboard/DashboardPage'));
