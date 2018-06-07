05ca5cdd6311c8a11db37a00596295e7ac15c4db
roy <roy@digitalxi.com>
2018-06-07 08:44:17 -0400

    setup global variables

--- src/shared/styles/colors.js
+++ src/shared/styles/colors.js
@@ -1,3 +1,6 @@
-export const grey100 = '#f5f5f5';
-export const grey200 = '#bdbdbd';
-export const grey300 = '#212121';
+export const whiteColor = '#FFFFFF';
+export const blackColor = '#000000';
+export const blueColor = '#2A75F6';
+export const lightBlueColor = '#F1F7FE';
+export const greyColor = '#E6EAEE';
+export const lightGreyColor = '#ACB8C5';
--- src/shared/styles/global.js
+++ src/shared/styles/global.js
@@ -1,5 +1,4 @@
 import { injectGlobal } from 'styled-components';
-import { grey300 } from './colors';
 
 /* eslint no-unused-expressions: 0 */
 injectGlobal`
@@ -8,11 +7,7 @@ injectGlobal`
     margin: 0;
     padding: 0;
     height: 100%;
-    width: 100%;
-    
-    color: ${grey300};
-    font-size: 14px;
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    width: 100%;    
   }
   
   #root {
