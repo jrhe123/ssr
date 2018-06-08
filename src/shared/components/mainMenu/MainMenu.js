477c3dc130471c93e3321aa7e8ac5383285813b6
roy <roy@digitalxi.com>
2018-06-08 12:04:37 -0400

    format nav bar

--- src/shared/components/mainMenu/MainMenu.js
+++ src/shared/components/mainMenu/MainMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // Libraries
 import AppBar from 'material-ui/AppBar';
@@ -7,18 +7,24 @@ import Toolbar from 'material-ui/Toolbar';
 // router
 import { Link } from 'react-router-dom';
 
-const MainMenu = () => (
-    <AppBar 
-        position="static" 
-        color="primary"
-    >
-        <Toolbar>
-            <nav style={styles.navBar}>
-                <Link to="/">Home</Link>
-            </nav>
-        </Toolbar>
-    </AppBar>
-);
+class MainMenu extends Component {
+
+    render() {
+
+        return (
+            <AppBar
+                position="static"
+                color="primary"
+            >
+                <Toolbar>
+                    <nav style={styles.navBar}>
+                        <Link to="/">Home</Link>
+                    </nav>
+                </Toolbar>
+            </AppBar>
+        )
+    }
+}
 
 const styles = {
 
