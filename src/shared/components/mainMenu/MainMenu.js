b7704d144d32012452d043679c70606dd4d39e5e
roy <roy@digitalxi.com>
2018-06-08 12:12:14 -0400

    add tab nav

--- src/shared/components/mainMenu/MainMenu.js
+++ src/shared/components/mainMenu/MainMenu.js
@@ -4,24 +4,55 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 
+import Tabs, { TabPane } from 'rc-tabs';
+import TabContent from 'rc-tabs/lib/TabContent';
+import ScrollableInkTabBar from 'rc-tabs/lib/ScrollableInkTabBar';
+import 'rc-tabs/assets/index.css';
+
 // router
 import { Link } from 'react-router-dom';
 
 class MainMenu extends Component {
 
+    state = {
+        start: 0,
+        tabKey: 1,
+    };
+
+    onTabClick = (key) => {
+        console.log(`onTabClick ${key}`);
+        this.setState({
+            tabKey: key,
+        });
+    }
+
     render() {
 
         return (
-            <AppBar
-                position="static"
-                color="primary"
+            <Tabs
+                defaultActiveKey={'2'}
+                renderTabBar={
+                    () => 
+                    <ScrollableInkTabBar onTabClick={this.onTabClick} />
+                }
+                renderTabContent={
+                    () => 
+                    <TabContent />
+                }
             >
-                <Toolbar>
-                    <nav style={styles.navBar}>
-                        <Link to="/">Home</Link>
-                    </nav>
-                </Toolbar>
-            </AppBar>
+                <TabPane 
+                    tab={`tab 1`} 
+                    key="3"
+                >
+                    tab 1 content
+                </TabPane>
+                <TabPane 
+                    tab={`tab 2`} 
+                    key="2"
+                >
+                    tab 2 content
+                </TabPane>
+            </Tabs>
         )
     }
 }
