f7025c47f4d01f127228b661425fef575a898064
roy <roy@digitalxi.com>
2018-06-08 08:36:48 -0400

    split the style

--- src/shared/components/alertBar/AlertBar.js
+++ src/shared/components/alertBar/AlertBar.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import Alert from 'react-s-alert';
 import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/jelly.css';
-import '../../../../assets/css/index.css';
+import '../../../../assets/css/react-s-alert.css';
 
 class AlertBar extends Component {
 
