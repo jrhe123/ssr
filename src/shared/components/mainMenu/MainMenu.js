14b9e3c1a7302332ad39da6f3564dc25b6277d0f
roy <roy@digitalxi.com>
2018-06-08 09:33:12 -0400

    header nav bar

--- src/shared/components/mainMenu/MainMenu.js
+++ src/shared/components/mainMenu/MainMenu.js
@@ -1,36 +1,31 @@
 import React from 'react';
+
+// Libraries
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
+
+// router
 import { Link } from 'react-router-dom';
 
-const navBar = {
-    display: 'block',
-    position: 'absolute',
-    right: '1rem',
-};
-
-const navLink = {
-    margin: '1rem',
-    paddingRight: '2rem',
-    textDecoration: 'none',
-    color: 'white',
-    fontSize: '2rem',
-};
-
 const MainMenu = () => (
-    <AppBar position="static" color="primary">
+    <AppBar 
+        position="static" 
+        color="primary"
+    >
         <Toolbar>
-            <Typography type="title" color="inherit">
-                Share easly your playlists from Spotify
-            </Typography>
-            <nav style={navBar}>
-                <Link style={navLink} to="/">Home</Link>
-                <Link style={navLink} to="/playlists">Your playlists</Link>
-                <Link style={navLink} to="/search-album">Search an album</Link>
+            <nav style={styles.navBar}>
+                <Link to="/">Home</Link>
             </nav>
         </Toolbar>
     </AppBar>
 );
+
+const styles = {
+
+    navBar: {
+        display: 'block',
+        position: 'fixed',
+    }
+}
 
 export default MainMenu;
