3277d7464e1f7195804390f16420b2ca18553c2c
roy <roy@digitalxi.com>
2018-06-11 14:51:17 -0400

    add font

--- src/server/render.js
+++ src/server/render.js
@@ -7,6 +7,7 @@ export const renderHeader = helmet => `
             ${helmet.meta.toString()}
             ${helmet.link.toString()}
             <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
             <link rel="icon" type="image/png" href="/assets/favicon.ico" />
         </head>
         <body ${helmet.bodyAttributes.toString()}>
