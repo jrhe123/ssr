af1cafc9120cefdc96efac03e2c7c1a1ebb3c534
roy <roy@digitalxi.com>
2018-06-12 11:50:41 -0400

    experience test

--- src/shared/dashboard/components/container/ExperienceList.js
+++ src/shared/dashboard/components/container/ExperienceList.js
@@ -3,19 +3,57 @@ import React, { Component } from 'react';
 // components
 import SearchBar from '../../../components/searchBar/SearchBar';
 
-class ExperienceList extends Component{
+class ExperienceList extends Component {
 
-    render(){
-        return(
+    render() {
+
+        const {
+            searchBarContainerStyle,
+            experiencelistContainerStyle,
+            experienceSortContainerStyle,
+            experiencelistContentContainerStyle,
+        } = styles;
+
+        return (
             <div>
-                <SearchBar 
-                    isShort={false}
-                    placeholder="search for card(s) and page(s)"
-                />
-
+                <div style={searchBarContainerStyle}>
+                    <SearchBar
+                        isShort={false}
+                        placeholder="search for card(s) and page(s)"
+                    />
+                </div>
+                <div style={experiencelistContainerStyle}>
+                    <div style={experienceSortContainerStyle}>
+                        1 Experience(s)
+                    </div>
+                    <div style={experiencelistContentContainerStyle}>
+                        {/* list of experiences */}
+                        <div>card</div>
+                        <div>flow</div>
+                        <div>status</div>
+                        {/* new experience */}
+                        <div>
+                            <img />
+                            <p>ADD EXPERIENCE</p>    
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
+}
+
+const styles = {
+
+    searchBarContainerStyle: {
+
+    },
+    experiencelistContainerStyle: {
+
+    },
+    experienceSortContainerStyle: {
+
+    },
 }
 
 export default ExperienceList;
