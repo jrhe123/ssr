a4977fbee7d637e59d0273e5eced1d121222d493
roy <roy@digitalxi.com>
2018-06-12 14:13:02 -0400

    test

--- src/shared/dashboard/components/layout/Experience.js
+++ src/shared/dashboard/components/layout/Experience.js
@@ -3,23 +3,38 @@ import React, { Component } from 'react';
 // component
 import ExperienceList from '../container/ExperienceList';
 
-class Experience extends Component{
+// Libraries
+import Button from '@material-ui/core/Button';
+import colors from '../../../styles/colors';
+import HelpOutline from '@material-ui/icons/HelpOutline';
 
-    render(){
+class Experience extends Component {
+
+    render() {
 
         const {
             mainContainerStyle,
             contentContainerStyle,
             newContainerStyle,
+            newBtnContainerStyle,
+            btnStyle,
+            iconStyle,
         } = styles;
 
-        return(
+        return (
             <div style={mainContainerStyle}>
                 <div style={contentContainerStyle}>
                     <ExperienceList />
                 </div>
                 <div style={newContainerStyle}>
-                    add btn
+                    <div style={newBtnContainerStyle}>
+                        <Button
+                            style={btnStyle}
+                            variant="Add new experience">
+                            Add Experience
+                        </Button>
+                        <HelpOutline style={iconStyle}/>
+                    </div>
                 </div>
             </div>
         )
@@ -37,6 +52,22 @@ const styles = {
     newContainerStyle: {
         flex: 1
     },
+    newBtnContainerStyle: {
+        marginTop: 48,
+        position: 'relative',
+    },
+    btnStyle: {
+        backgroundColor: colors.blueColor,
+        color: colors.whiteColor,
+        textTransform: 'capitalize',
+        width: 132
+    },
+    iconStyle: {
+        position: 'absolute',
+        top: 6,
+        left: 138,
+        cursor: 'pointer'
+    }
 }
 
 export default Experience;
