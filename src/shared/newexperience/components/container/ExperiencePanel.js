2a289cb046524cdf4cee9c2829c6195ef476d48b
roy <roy@digitalxi.com>
2018-06-13 13:40:20 -0400

    experience panel

--- src/shared/newexperience/components/container/ExperiencePanel.js
+++ src/shared/newexperience/components/container/ExperiencePanel.js
@@ -1,16 +1,65 @@
 import React, { Component } from 'react';
 
-class ExperiencePanel extends Component{
+// Libraries
+import Button from '@material-ui/core/Button';
 
-    render(){
+// constants
+import colors from '../../../styles/colors';
+
+class ExperiencePanel extends Component {
+
+    render() {
 
         const {
             mainContainerStyle,
+            optionContainerStyle,
+            leftContainerStyle,
+            rightContainerStyle,
+            btnStyle,
         } = styles;
 
-        return(
+        return (
             <div style={mainContainerStyle}>
-                panel here
+                <div style={optionContainerStyle}>
+                    <div style={leftContainerStyle}>
+                        <p>Type</p>
+                    </div>
+                    <div style={rightContainerStyle}>
+                        <p>Select what kind of experience you would like your end user to experience?</p>
+                    </div>
+                </div>
+                <div style={optionContainerStyle}>
+                    <div style={leftContainerStyle}>
+                        <img
+                            src={require('../../../../../assets/images/card_option.png')}
+                        />
+                        <span>Card</span>
+                    </div>
+                    <div style={rightContainerStyle}>
+                        <p>Cards are the entry point to your end user's experience</p>
+                        <Button
+                            style={btnStyle}
+                            variant="Create card">
+                            Create a card
+                        </Button>
+                    </div>
+                </div>
+                <div style={optionContainerStyle}>
+                    <div style={leftContainerStyle}>
+                        <img
+                            src={require('../../../../../assets/images/card_option.png')}
+                        />
+                        <span>Page(s)</span>
+                    </div>
+                    <div style={rightContainerStyle}>
+                        <p>Page(s) are the follow-up screens after the end user clicked the above card.<br />Multiple page(s) are linked via sections.</p>
+                        <Button
+                            style={btnStyle}
+                            variant="Create pages">
+                            Create page(s)
+                        </Button>
+                    </div>
+                </div>
             </div>
         )
     }
@@ -23,7 +72,22 @@ const styles = {
         margin: '0 auto',
         border: '1px solid red'
     },
-
+    optionContainerStyle: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    leftContainerStyle: {
+        flex: 1
+    },
+    rightContainerStyle: {
+        flex: 3
+    },
+    btnStyle: {
+        backgroundColor: colors.blueColor,
+        color: colors.whiteColor,
+        textTransform: 'capitalize',
+        width: 132
+    }
 }
 
 export default ExperiencePanel;
