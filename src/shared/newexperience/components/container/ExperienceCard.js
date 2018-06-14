b9fbcd6a13ce74e12eb023706e5608eaa59bf547
roy <roy@digitalxi.com>
2018-06-14 11:15:25 -0400

    setup swipe

---
+++ src/shared/newexperience/components/container/ExperienceCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ExperienceCard extends Component{
+
+    render(){
+
+        return(
+            <div>experience card here</div>
+        )
+    }
+}
+
+export default ExperienceCard;
--- src/shared/newexperience/components/container/ExperienceControl.js
+++ src/shared/newexperience/components/container/ExperienceControl.js
@@ -14,6 +14,7 @@ import sizes from '../../../styles/sizes';
 
 // components
 import ExperiencePanel from './ExperiencePanel';
+import ExperienceCard from './ExperienceCard';
 
 class ExperienceControl extends Component {
 
@@ -50,9 +51,7 @@ class ExperienceControl extends Component {
                 {
                     experienceIndex == 1 ?
                         (
-                            <div style={{ border: '1px solid red' }}>
-                                <a onClick={() => this.handleChange(0)}>click</a>
-                            </div>
+                            <ExperienceCard />
                         )
                         :
                         null
