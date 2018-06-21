2b927b21b06a1ee0d1255ccbd0f974c4ca0b600d
roy <roy@digitalxi.com>
2018-06-21 14:09:32 -0400

    test

--- src/shared/newexperience/components/container/Knight.js
+++ src/shared/newexperience/components/container/Knight.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react'
-import { ItemTypes } from '../../constants'
-import { DragSource } from 'react-dnd'
-import knightImage from './knight.png'
+import React, { Component } from 'react';
+import { ItemTypes } from '../../constants';
+import { DragSource } from 'react-dnd';
 
 const knightSource = {
     beginDrag(props, dnd, element) {
-        // console.log('props of knight, since these aren\'t in the docs')
-        // console.log(props, dnd, element)
         return {}
     }
 }
@@ -14,18 +11,11 @@ const knightSource = {
 function collect(connect, monitor) {
     return {
         connectDragSource: connect.dragSource(),
-        connectDragPreview: connect.dragPreview(),
         isDragging: monitor.isDragging()
     }
 }
 
 class Knight extends Component {
-
-    componentDidMount() {
-        const img = new Image();
-        img.src = knightImage
-        // this.props.connectDragPreview(img);
-    }
 
     render() {
         const { connectDragSource, isDragging } = this.props
