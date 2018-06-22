14ec5fdc43bbf575f91406ad72475f74149ba996
roy <roy@digitalxi.com>
2018-06-22 09:57:22 -0400

    page elem style

--- src/shared/newexperience/components/presentation/PageTemplate.js
+++ src/shared/newexperience/components/presentation/PageTemplate.js
@@ -28,18 +28,86 @@ function collect(connect, monitor) {
 class PageTemplate extends Component {
 
     renderPage = (template) => {
+
+        const {
+            tableContainerStyle,
+            tableWrapperStyle,
+            leftImageContainerStyle,
+            rightTextContainerStyle,
+            imgStyle,
+            txtStyle,
+            btnPageContainerStyle,
+            topBtnContainerStyle,
+            btnLabelContainerStyle,
+            btnLabelStyle,
+            bottomDescContainerStyle,
+        } = styles;
+
         let card;
         if (template.Type == 'EDITOR') {
             card = (
-                <div>EDITOR</div>
+                <div>
+                    <div style={leftImageContainerStyle}>
+                        <div style={tableContainerStyle}>
+                            <div style={tableWrapperStyle}>
+                                <img
+                                    style={imgStyle}
+                                    src={require('../../../../../assets/images/demo.jpg')}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div style={rightTextContainerStyle}>
+                        <div style={tableContainerStyle}>
+                            <div style={Object.assign({}, tableWrapperStyle, { textAlign: 'left' })}>
+                                <p style={txtStyle}>Embed a full featured responsive publishXi editor directly in the page</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             )
         } else if (template.Type == 'BUTTON') {
             card = (
-                <div>BUTTON</div>
+                <div style={btnPageContainerStyle}>
+                    <div style={topBtnContainerStyle}>
+                        <div style={tableContainerStyle}>
+                            <div style={tableWrapperStyle}>
+                                <div style={btnLabelContainerStyle}>
+                                    <p style={btnLabelStyle}>Text for a button</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={bottomDescContainerStyle}>
+                        <div style={tableContainerStyle}>
+                            <div style={tableWrapperStyle}>
+                                <p style={txtStyle}>Connect another page with this button</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             )
         } else if (template.Type == 'EMBED_PDF') {
             card = (
-                <div>EMBED_PDF</div>
+                <div>
+                    <div style={leftImageContainerStyle}>
+                        <div style={tableContainerStyle}>
+                            <div style={tableWrapperStyle}>
+                                <img
+                                    style={imgStyle}
+                                    src={require('../../../../../assets/images/demo.jpg')}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div style={rightTextContainerStyle}>
+                        <div style={tableContainerStyle}>
+                            <div style={Object.assign({}, tableWrapperStyle, { textAlign: 'left' })}>
+                                <p style={txtStyle}>Choose this page element to embed and show a PDF file on the page directly to the user</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             )
         } else if (template.Type == 'SPLASH') {
             card = (
@@ -63,15 +131,13 @@ class PageTemplate extends Component {
 
         const {
             mainContainerStyle,
-            tableContainerStyle,
-            tableWrapperStyle,
             pageContainerStyle,
         } = styles;
 
         return connectDragSource(
             <div style={mainContainerStyle}>
                 <div style={Object.assign({}, pageContainerStyle, { opacity })}
-                    className="dx_card"
+                    className="dx_page"
                 >
                     {this.renderPage(template)}
                 </div>
@@ -102,6 +168,50 @@ const styles = {
         height: 90,
         cursor: 'pointer',
         margin: '0 auto'
+    },
+    leftImageContainerStyle: {
+        display: 'inline-block',
+        float: 'left',
+        width: 90,
+        height: 90,
+    },
+    rightTextContainerStyle: {
+        display: 'inline-block',
+        float: 'left',
+        width: 'calc(100% - 90px)',
+        height: 90,
+    },
+    imgStyle: {
+        display: 'block',
+        width: 60,
+        height: 60,
+        margin: '0 auto'
+    },
+    txtStyle: {
+        fontSize: fonts.h5
+    },
+    btnPageContainerStyle: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: 90,
+    },
+    topBtnContainerStyle: {
+        flex: 2,
+    },
+    btnLabelContainerStyle: {
+        width: 'calc(100% - 24px)',
+        margin: '0 auto',
+        backgroundColor: colors.greyColor,
+        paddingTop: 6,
+        paddingBottom: 6
+    },
+    btnLabelStyle: {
+
+    },
+    bottomDescContainerStyle: {
+        flex: 1,
+        paddingLeft: 6,
+        paddingRight: 6,
     },
 }
 
