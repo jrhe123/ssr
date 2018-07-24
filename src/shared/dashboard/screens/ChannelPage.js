2299c7876ae51129b5b0309a5822bc6ddb80c362
Dhaneswar Pandian <dhan@digitalxi.com>
2018-07-24 07:46:05 -0400

    Channel missed screen{

--- src/shared/dashboard/screens/ChannelPage.js
+++ src/shared/dashboard/screens/ChannelPage.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 // components
 import Channel from '../components/layout/Channel';
-import ChannelListTemplate from '../components/presentation/ChannelListTemplate';
+// import ChannelListTemplate from '../components/presentation/ChannelListTemplate';
 
 class ChannelPage extends Component{
 
     render(){
 
         return(
-            <ChannelListTemplate />
+            <Channel />
         )
 
     }
