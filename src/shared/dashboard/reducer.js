815fa86f162c7635c3a814b38e2310292d99c496
roy <roy@digitalxi.com>
2018-07-27 13:03:06 -0400

    load html page

--- src/shared/dashboard/reducer.js
+++ src/shared/dashboard/reducer.js
@@ -8,20 +8,27 @@ const initialState = {
     TotalExperienceRecord: 0,
     Experiences: [],
 
-    Channels:[]
+    Channels: []
 };
 
 const dashboardReducer = (previousState = initialState, { type, payload }) => {
-    
+
     let updated = Object.assign({}, previousState);
-    let tmpExperiences;
+    let tmpExperiences = Object.assign([], updated.Experiences);
+
+    let tmpExperience;
     let tmpPage;
     let tmpSection;
 
     switch (type) {
 
         case HTML_FETCH__SUCCEEDED:
-            console.log('reducer received: ', payload);
+            tmpExperience = __find_experience_by_guid(updated.Experiences, payload.experienceGUID);
+            tmpPage = __find_page_by_guid(tmpExperience.experience.ExperiencePages, payload.pageGUID);
+            tmpSection = __find_section_by_guid(tmpPage.page.Sections, payload.sectionGUID);
+            tmpPage.page.Sections[tmpSection.index].HtmlContent = payload.html;
+            tmpExperiences[tmpExperience.index] = tmpExperience.experience;
+            updated.Experiences = tmpExperiences;
             return updated;
 
         case CHANNEL_FETCH__SUCCEEDED:
@@ -37,5 +44,44 @@ const dashboardReducer = (previousState = initialState, { type, payload }) => {
             return previousState;
     }
 };
+
+const __find_experience_by_guid = (experiences, guid) => {
+    for (let i = 0; i < experiences.length; i++) {
+        let experience = experiences[i];
+        if (experience.ExperienceGUID == guid) {
+            return {
+                index: i,
+                experience,
+            };
+        }
+    }
+    return null;
+}
+
+const __find_page_by_guid = (pages, guid) => {
+    for (let i = 0; i < pages.length; i++) {
+        let page = pages[i];
+        if (page.PageGUID == guid) {
+            return {
+                index: i,
+                page,
+            };
+        }
+    }
+    return null;
+}
+
+const __find_section_by_guid = (sections, guid) => {
+    for (let i = 0; i < sections.length; i++) {
+        let section = sections[i];
+        if (section.SectionGUID == guid) {
+            return {
+                index: i,
+                section,
+            };
+        }
+    }
+    return null;
+}
 
 export default dashboardReducer;
