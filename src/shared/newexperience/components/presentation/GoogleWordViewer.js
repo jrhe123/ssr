eb540892cfcf059794912360b5c418caa56aa283
roy <roy@digitalxi.com>
2018-07-31 15:24:21 -0400

    doc viwer

--- src/shared/newexperience/components/presentation/GoogleWordViewer.js
+++ src/shared/newexperience/components/presentation/GoogleWordViewer.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 
+// constants
+import colors from '../../../styles/colors';
+
 class GoogleWordViewer extends Component {
 
     render() {
-
         const {
             fileID
         } = this.props;
-
-        console.log('fileID: ', fileID);
+        const {
+            mainContainerStyle,
+            coverFrameStyle,
+        } = styles;
 
         return (
-            <div>viewer here</div>
+            <div style={mainContainerStyle}>
+                <Iframe
+                    url={`https://drive.google.com/open?id=${fileID}`}
+                    width="100%"
+                    height="500px"
+                    display="initial"
+                    position="relative"
+                    allowFullScreen>
+                </Iframe>
+                <div style={coverFrameStyle} />
+            </div>
         )
     }
+}
+
+const styles = {
+
+    mainContainerStyle: {
+        position: 'relative',
+        border: '1px solid red',
+    },
+    coverFrameStyle: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: 36,
+        width: "100%",
+        background: colors.whiteColor
+    },
 }
 
 export default GoogleWordViewer;
