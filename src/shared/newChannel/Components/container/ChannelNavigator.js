c8a8c11a30fe66ad7c2192501319a5cdeab8c1af
roy <roy@digitalxi.com>
2018-08-02 11:13:47 -0400

    test

--- src/shared/newChannel/Components/container/ChannelNavigator.js
+++ src/shared/newChannel/Components/container/ChannelNavigator.js
@@ -2,28 +2,21 @@ import React, { Component } from 'react';
 
 // components
 import NavBar from '../../../components/navBar/NavBar';
-import DxInput from '../../../components/dxInput/DxInput';
 
 // redux
 import { connect } from 'react-redux';
 
 class ChannelNavigator extends Component {
 
-    handleTitleChange = (e) => {
-
-    }
-
     handleGoback = () => {
         this.props.history.push('/dashboard');
     }
 
     render() {
-
         return (
             <NavBar
                 isRoute={false}
                 navType="CHANNEL"
-                handleInputChange={(e) => this.handleTitleChange(e)}
                 handleGoback={() => this.handleGoback()}
             />
         )
--- src/shared/newChannel/Components/container/ChannelPanel.js
+++ src/shared/newChannel/Components/container/ChannelPanel.js
@@ -23,11 +23,10 @@ import {
 import colors from '../../../styles/colors';
 import fonts from '../../../styles/fonts';
 
-// // components
+// components
 import DxInput from '../../../components/dxInput/DxInput';
 
-
-class ChannelPanel extends Component{
+class ChannelPanel extends Component {
 
     state = {
         isTypeMenuOpen: false,
@@ -38,17 +37,17 @@ class ChannelPanel extends Component{
     }
 
     toggleTypeSelect = () => {
-        this.setState({ 
-            isTypeMenuOpen: !this.state.isTypeMenuOpen 
+        this.setState({
+            isTypeMenuOpen: !this.state.isTypeMenuOpen
         });
     }
 
     toggleColorSelect = () => {
-        this.setState({ 
-            isColorMenuOpen: !this.state.isColorMenuOpen 
+        this.setState({
+            isColorMenuOpen: !this.state.isColorMenuOpen
         });
     }
-    
+
     closeTypeSelect = () => {
         this.setState({ isTypeMenuOpen: false });
     }
@@ -58,43 +57,37 @@ class ChannelPanel extends Component{
     }
 
     colorBlueOptionclick = () => {
-        this.setState({ btnPickerColor: '#4A90E2'});
+        this.setState({ btnPickerColor: '#4A90E2' });
         this.setState({ isColorMenuOpen: false });
-        console.log('btnPickerColor', this.state.btnPickerColor);
     }
 
     colorYellowOptionclick = () => {
-        this.setState({ btnPickerColor: '#DFA92E'});
+        this.setState({ btnPickerColor: '#DFA92E' });
         this.setState({ isColorMenuOpen: false });
-        console.log('btnPickerColor', this.state.btnPickerColor);
     }
 
     colorPurpleOptionclick = () => {
-        this.setState({ btnPickerColor: '#913D88'});
+        this.setState({ btnPickerColor: '#913D88' });
         this.setState({ isColorMenuOpen: false });
-        console.log('btnPickerColor', this.state.btnPickerColor);
     }
 
     colorGreenOptionclick = () => {
-        this.setState({ btnPickerColor: '#1AA98B'});
+        this.setState({ btnPickerColor: '#1AA98B' });
         this.setState({ isColorMenuOpen: false });
-        console.log('btnPickerColor', this.state.btnPickerColor);
     }
 
     colorOrangeOptionclick = () => {
-        this.setState({ btnPickerColor: '#EC5C03'});
+        this.setState({ btnPickerColor: '#EC5C03' });
         this.setState({ isColorMenuOpen: false });
-        console.log('btnPickerColor', this.state.btnPickerColor);
     }
 
     colorGreyOptionclick = () => {
-        this.setState({ btnPickerColor: '#83909B'});
+        this.setState({ btnPickerColor: '#83909B' });
         this.setState({ isColorMenuOpen: false });
     }
 
     handleColorPicker = (obj) => {
-        this.setState({ btnPickerColor: obj.color});
-        console.log('btnPickerColor', this.state.btnPickerColor);
+        this.setState({ btnPickerColor: obj.color });
         this.props.dxChannelColorAction(obj.color)
     }
 
@@ -103,24 +96,20 @@ class ChannelPanel extends Component{
         this.setState({
             titleCharacterCount: 0 + input.length
         });
-
     }
 
     handleDescriptionCharacterChange = () => {
         var input = event.target.value;
         this.setState({
             descriptionCharacterCount: 0 + input.length
-        });   
+        });
     }
 
     handleClickOption = (val) => {
         this.props.dxChannelTypeUpdateAction(val);
     }
 
-    render(){
-        const {
-            classes
-        } = this.props;     
+    render() {
 
         const {
             mainContainerStyle,
@@ -142,10 +131,8 @@ class ChannelPanel extends Component{
         const {
             channel
         } = this.props;
-        
-        console.log('does this match',channel.COLOR)
 
-        return(
+        return (
             <div style={mainContainerStyle}>
                 <div style={optionContainerStyle}>
                     <div style={leftContainerStyle}>
@@ -158,13 +145,13 @@ class ChannelPanel extends Component{
                             close={this.closeTypeSelect}
                             toggle={
                                 <div>
-                                    <DxInput 
+                                    <DxInput
                                         placeholder="type"
                                         width="226px"
                                         disabled={true}
                                         value={channel.TYPE == 0 ? 'Public Channel' : 'Private Channel - Invite only 🔒'}
                                     />
-                                    <Button 
+                                    <Button
                                         style={outlineBtnStyle}
                                         onClick={() => this.toggleTypeSelect()}>
                                         EDIT
@@ -174,17 +161,17 @@ class ChannelPanel extends Component{
                             align='left'
                         >
                             <div onClick={() => this.handleClickOption(0)}>
-                                <Button 
+                                <Button
                                     style={optionBtnStyle}
                                     className="dx-lower-case"
                                 >Public Channel</Button>
                             </div>
                             <div onClick={() => this.handleClickOption(1)}>
-                                <Button 
+                                <Button
                                     style={optionBtnStyle}
                                     className="dx-lower-case"
                                 >
-                                    <p style={privateChannelLabel}>Private Channel - Invite only<Lock style={lockStyle}/></p>
+                                    <p style={privateChannelLabel}>Private Channel - Invite only<Lock style={lockStyle} /></p>
                                 </Button>
                             </div>
                         </DropdownMenu>
@@ -196,7 +183,7 @@ class ChannelPanel extends Component{
                     </div>
                     <div style={rightContainerStyle}>
                         <p>Choose a color for your channel. We recommend using one color for each channel you own.</p>
-                        
+
                         <DropdownMenu
                             isOpen={this.state.isColorMenuOpen}
                             close={this.closeColorSelect}
@@ -204,9 +191,9 @@ class ChannelPanel extends Component{
                             align='left'
                             toggle={
                                 <div>
-                                    <IconButton 
-                                        style={{backgroundColor:this.state.btnPickerColor, height:20, width:20}} 
-                                        iconStyle={{height:5, width:5}} 
+                                    <IconButton
+                                        style={{ backgroundColor: this.state.btnPickerColor, height: 20, width: 20 }}
+                                        iconStyle={{ height: 5, width: 5 }}
                                         onClick={() => this.toggleColorSelect()}
                                     />
                                 </div>
@@ -217,43 +204,43 @@ class ChannelPanel extends Component{
                             <div style={colorOptionContainerStyle}>
                                 <div style={colorOptionStyle}>
                                     <IconButton
-                                        style={{backgroundColor:'#4A90E2', height:20, width:20}} 
-                                        iconStyle={{height:5, width:5}} 
+                                        style={{ backgroundColor: '#4A90E2', height: 20, width: 20 }}
+                                        iconStyle={{ height: 5, width: 5 }}
                                         onClick={this.colorBlueOptionclick}
                                     />
                                 </div>
                                 <div style={colorOptionStyle}>
-                                    <IconButton 
-                                        style={{backgroundColor:'#DFA92E', height:20, width:20}} 
-                                        iconStyle={{height:5, width:5}}
+                                    <IconButton
+                                        style={{ backgroundColor: '#DFA92E', height: 20, width: 20 }}
+                                        iconStyle={{ height: 5, width: 5 }}
                                         onClick={this.colorYellowOptionclick}
                                     />
                                 </div>
                                 <div style={colorOptionStyle}>
-                                    <IconButton 
-                                        style={{backgroundColor:'#913D88', height:20, width:20}} 
-                                        iconStyle={{height:5, width:5}}
+                                    <IconButton
+                                        style={{ backgroundColor: '#913D88', height: 20, width: 20 }}
+                                        iconStyle={{ height: 5, width: 5 }}
                                         onClick={this.colorPurpleOptionclick}
                                     />
                                 </div>
                                 <div style={colorOptionStyle}>
-                                    <IconButton 
-                                        style={{backgroundColor:'#1AA98B', height:20, width:20}} 
-                                        iconStyle={{height:5, width:5}}
+                                    <IconButton
+                                        style={{ backgroundColor: '#1AA98B', height: 20, width: 20 }}
+                                        iconStyle={{ height: 5, width: 5 }}
                                         onClick={this.colorGreenOptionclick}
                                     />
                                 </div>
                                 <div style={colorOptionStyle}>
-                                    <IconButton 
-                                        style={{backgroundColor:'#EC5C03', height:20, width:20}} 
-                                        iconStyle={{height:5, width:5}}
+                                    <IconButton
+                                        style={{ backgroundColor: '#EC5C03', height: 20, width: 20 }}
+                                        iconStyle={{ height: 5, width: 5 }}
                                         onClick={this.colorOrangeOptionclick}
                                     />
                                 </div>
                                 <div style={colorOptionStyle}>
-                                    <IconButton 
-                                        style={{backgroundColor:'#83909B', height:20, width:20}} 
-                                        iconStyle={{height:5, width:5}}
+                                    <IconButton
+                                        style={{ backgroundColor: '#83909B', height: 20, width: 20 }}
+                                        iconStyle={{ height: 5, width: 5 }}
                                         onClick={this.colorGreyOptionclick}
                                     />
                                 </div>
@@ -265,7 +252,7 @@ class ChannelPanel extends Component{
                                     >
                                         <span className="react-custom-trigger" fullWidth>{channel.COLOR}</span>
                                     </ColorPicker>
-                                </div>                               
+                                </div>
                             </div>
                         </DropdownMenu>
                     </div>
@@ -276,22 +263,22 @@ class ChannelPanel extends Component{
                     </div>
                     <div style={rightContainerStyle}>
                         <p>Choose a channel title relevant to your audience’s interest. Ex. Diabetes, Radiology, News etc.</p>
-                        <input 
-                            type="text" 
-                            maxlength="50" 
+                        <input
+                            type="text"
+                            maxlength="50"
                             placeholder="Hypertension"
-                            style={{ 
-                                width:'96%', 
-                                padding:'12px 12px 12px 12px', 
-                                height:'36px', 
-                                fontSize:14, 
-                                color:channel.COLOR,
-                                border:'none',
-                                outlineStyle:'none',
-                                marginBottom:6,
+                            style={{
+                                width: '96%',
+                                padding: '12px 12px 12px 12px',
+                                height: '36px',
+                                fontSize: 14,
+                                color: channel.COLOR,
+                                border: 'none',
+                                outlineStyle: 'none',
+                                marginBottom: 6,
                                 boxShadow: '0 2px 4px 0 rgba(0,0,0,0.5)'
-                            }} 
-                            onChange={this.handleTitleCharacterChange.bind(this)}/>
+                            }}
+                            onChange={this.handleTitleCharacterChange.bind(this)} />
                         <p style={characterCounterStyle}>{this.state.titleCharacterCount}/50</p>
                     </div>
                 </div>
@@ -309,10 +296,10 @@ class ChannelPanel extends Component{
                             InputProps={{
                                 disableUnderline: true,
                             }}
-                            inputProps={{maxLength: 1000}}
+                            inputProps={{ maxLength: 1000 }}
                             style={textAreaStyle}
                             fullWidth
-                            placeholder= {'Type something'}
+                            placeholder={'Type something'}
                             onChange={this.handleDescriptionCharacterChange.bind(this)}
                         />
                         <p style={characterCounterStyle}>{this.state.descriptionCharacterCount}/1000</p>
@@ -326,28 +313,28 @@ class ChannelPanel extends Component{
 
 const styles = {
 
-    mainContainerStyle:{
+    mainContainerStyle: {
         width: 600,
         margin: '0 auto',
     },
-    optionContainerStyle:{
-        display:'flex',
+    optionContainerStyle: {
+        display: 'flex',
         flexDirection: 'row',
-        paddingTop:36,
+        paddingTop: 36,
     },
     leftContainerStyle: {
-        flex:1,
-        display:'flex',
+        flex: 1,
+        display: 'flex',
         flexDirection: 'row',
         fontSize: fonts.h3,
         color: colors.blackColor,
     },
     rightContainerStyle: {
-        flex:4,
+        flex: 4,
         color: colors.labelColor,
         fontSize: fonts.h4
     },
-    labelStyle:{
+    labelStyle: {
         textAlign: 'left',
     },
     outlineBtnStyle: {
@@ -356,46 +343,46 @@ const styles = {
     optionBtnStyle: {
         width: 144
     },
-    textAreaStyle:{
-        backgroundColor:'white',
-        paddingLeft:12,
-        paddingTop:12,
-        paddingRight:12,
-        paddingBottom:12,
-        width:'96%'
+    textAreaStyle: {
+        backgroundColor: 'white',
+        paddingLeft: 12,
+        paddingTop: 12,
+        paddingRight: 12,
+        paddingBottom: 12,
+        width: '96%'
     },
-    colorOptionStyle:{
-        paddingTop:6,
-        paddingLeft:12,
-        paddingBottom:6,
-        marginTop:0
+    colorOptionStyle: {
+        paddingTop: 6,
+        paddingLeft: 12,
+        paddingBottom: 6,
+        marginTop: 0
     },
-    colorOptionContainerStyle:{
-        display:'flex',
-        flexDirection:'row'
+    colorOptionContainerStyle: {
+        display: 'flex',
+        flexDirection: 'row'
     },
-    characterCounterStyle:{
-        display:'flex', 
-        flexDirection:'row',
+    characterCounterStyle: {
+        display: 'flex',
+        flexDirection: 'row',
         justifyContent: 'flex-end',
-        margin:0
+        margin: 0
     },
-    colorPickerStyle:{
-        margin: '6px 12px', 
-        paddingLeft:'6px', 
-        paddingRight:'6px', 
-        textAlign: 'Left', 
-        height:'20px', 
-        width:'64px', 
-        backgroundColor:'#F0F7FF', 
+    colorPickerStyle: {
+        margin: '6px 12px',
+        paddingLeft: '6px',
+        paddingRight: '6px',
+        textAlign: 'Left',
+        height: '20px',
+        width: '64px',
+        backgroundColor: '#F0F7FF',
         color: '#6A6875'
     },
-    lockStyle:{
-        height:'14px',
+    lockStyle: {
+        height: '14px',
         position: 'relative',
     },
-    privateChannelLabel:{
-        margin:0
+    privateChannelLabel: {
+        margin: 0
     }
 }
 
