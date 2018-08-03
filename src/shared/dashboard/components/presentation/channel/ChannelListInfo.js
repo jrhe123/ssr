7bc8e98a2082b6cb92bfad71575fc9279ff0af85
roy <roy@digitalxi.com>
2018-08-03 12:15:28 -0400

    test

--- src/shared/dashboard/components/presentation/channel/ChannelListInfo.js
+++ src/shared/dashboard/components/presentation/channel/ChannelListInfo.js
@@ -22,7 +22,7 @@ class ChannelListInfo extends Component {
             channelLabel,
             backgroundColor,
             isLive
-        }  = this.props;
+        } = this.props;
 
         const extra = {};
         if (isLive) {
@@ -36,14 +36,14 @@ class ChannelListInfo extends Component {
         return (
             <div style={channelInfoContainerStyle}>
                 <div style={channelInfo}>
-                    <IconButton 
-                        style={{backgroundColor: backgroundColor, height:15, width:15, marginLeft:12}} 
-                        iconStyle={{height:5, width:5}}
+                    <IconButton
+                        style={{ backgroundColor: backgroundColor, height: 15, width: 15, marginLeft: 12 }}
+                        iconStyle={{ height: 5, width: 5 }}
                     />
-                    <p style={{color: backgroundColor, marginLeft:9, fontSize:fonts.h4, marginRight:12}}>{channelLabel}</p>
+                    <p style={{ color: backgroundColor, marginLeft: 9, fontSize: fonts.h4, marginRight: 12 }}>{channelLabel}</p>
                 </div>
                 <div style={channelStatusContainerStyle}>
-                        <p style={Object.assign({}, styles.channelStatusStyle, extra)}>{extra.channelStatus}</p>
+                    <p style={Object.assign({}, styles.channelStatusStyle, extra)}>{extra.channelStatus}</p>
                 </div>
             </div>
         );
@@ -51,36 +51,37 @@ class ChannelListInfo extends Component {
 }
 
 const styles = {
-    channelInfoContainerStyle:{
-        display:'flex',
-        flexDirection:'column',
-        justifyContent:'flex-start',
-        alignItems:'flex-end'
+    channelInfoContainerStyle: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-end'
     },
-    channelInfo:{
-        height:36,
-        width:160,
-        background:colors.whiteColor,
-        marginRight:30,
-        marginTop:18,
-        display:'flex',
-        alignItems:'center'
+    channelInfo: {
+        height: 36,
+        width: 160,
+        background: colors.whiteColor,
+        marginRight: 30,
+        marginTop: 18,
+        display: 'flex',
+        alignItems: 'center',
+        cursor: 'pointer',
     },
-    channelStatusContainerStyle:{
-        width:60,
-        height:12,
-        fontSize:8,
-        background:colors.lightBlueColor,
-        textAlign:'center',
-        marginRight:30,
-        marginTop:6,
-        borderRadius:5.5,
-        display:'flex',
-        alignItems:'center',
-        justifyContent:'center'
+    channelStatusContainerStyle: {
+        width: 60,
+        height: 12,
+        fontSize: 8,
+        background: colors.lightBlueColor,
+        textAlign: 'center',
+        marginRight: 30,
+        marginTop: 6,
+        borderRadius: 5.5,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-    channelStatusStyle:{
-        margin:0
+    channelStatusStyle: {
+        margin: 0
     }
 }
 export default ChannelListInfo;
